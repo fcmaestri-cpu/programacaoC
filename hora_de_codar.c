2793030876e1191084bfc46c7f59fe52787b9bbc
Felipe Coelho Maestri <fcmaestri@gmail.com>
2025-09-17 15:21:11 -0300

    Update hora_de_codar.c

--- hora_de_codar.c
+++ hora_de_codar.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(){
+
+    char nome[50];
+    int idade;
+    int matricula;
+    float altura;
+
+    printf ("Informe seu nome: \n");
+    scanf ("%s", nome);
+
+    printf ("Informe sua idade: \n");
+    scanf ("%d", &idade);
+
+    printf ("Informe sua altura: \n");
+    scanf ("%f", &altura);
+
+    printf ("Informe sua matrícula: \n");
+    scanf ("%d", &matricula);
+    
+    printf ("%s tem %d anos, mede %.2fm e sua matricula é %d. \n", nome, idade, altura, matricula);
+
+
+}
